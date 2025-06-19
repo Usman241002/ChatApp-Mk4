9e760583ff68ebca6575f3afb7d263cf83a06809
Usman Khalid <ukhalid428@gmail.com>
2025-06-19 22:02:31 +0100

    Update package.json

--- client/src/app/socket.ts
+++ client/src/app/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io("http://localhost:8000", {
+export const socket: Socket = io(import.meta.env.VITE_SOCKET_SERVER_URL, {
   autoConnect: false,
 });
